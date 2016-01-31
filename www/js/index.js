ca55f2a6b84c7c0d3685e7cea18a27745432aff3
Youssef KAAOUACHI <ykaaouachi@gmail.com>
2016-01-31 16:55:00 +0000

    update all link target 2

--- www/js/index.js
+++ www/js/index.js
@@ -18,14 +18,14 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         $('a').attr('target', '_parent');
         /*document.addEventListener('click', function(e) {
